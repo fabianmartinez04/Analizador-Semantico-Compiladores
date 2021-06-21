28fd413c820edb3033f90b67225b7f5f85de3a47
Pablo Alpizar <pabloalpizar99@gmail.com>
2021-06-20 23:18:37 -0600

    add semantic stack and some semantic register

---
+++ AnalizadorSemantico/src/Estructuras/SemanticStack.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estructuras;
+
+import Estructuras.SematicRegister.RS;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/**
+ *
+ * @author pablo
+ */
+public class SemanticStack {
+  LinkedList<RS> stack;
+  
+  
+  
+  // remove and return the element in the last position of the list
+  RS pop() {
+    return this.stack.removeLast();
+  }
+  
+  // add element in last position of the list
+  void push(RS r) {
+      this.stack.addLast(r);
+  }
+  
+ // searching a register in entry list
+ /*RS find() {
+     ListIterator<RS> iterator = this.stack.listIterator();
+     RS r = null;
+     while (iterator.hasNext()) {
+         r = iterator.next();
+         if (r.name.equals())
+             return r;
+     }
+     return r;
+ }*/
+}
