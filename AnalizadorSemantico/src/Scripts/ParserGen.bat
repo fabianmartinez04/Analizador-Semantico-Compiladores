3ca00592346f7b4f5661061ee94a790425fa4631
Pablo Alpizar <pabloalpizar99@gmail.com>
2021-06-22 15:29:42 -0600

    update script

--- AnalizadorSemantico/src/Scripts/ParserGen.bat
+++ AnalizadorSemantico/src/Scripts/ParserGen.bat
@@ -1,6 +1,6 @@
 SET JAVA_HOME = "C:\Program Files\Java\jre1.8.0_271\bin"
 SET PATH=%JAVA_HOME%;%PATH%
 SET CLASSPATH=%JAVA_HOME%;
-cd "D:\Desktop\CI\Proyectos\Etapa 2\Analizador-Semantico-Compiladores\AnalizadorSemantico\src\codigo"
+cd "D:\Desktop\CI\Proyectos\Etapa 3\Analizador-Semantico-Compiladores\AnalizadorSemantico\src\codigo"
 java -jar "D:\Desktop\CI\Proyectos\Etapa 3\Analizador-Semantico-Compiladores\lib\java-cup-11b.jar" -parser Parser -symbols Sym Parser.cup
 pause
